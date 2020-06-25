2c2ab2d6751bbe6d0b6ce36230b5bdf8dee77b14
TheKOMX <thekom@hotmail.com>
2020-06-25 23:46:45 +0800

    comments added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,32 +1,43 @@
 import os as OS
 import csv as CSV
 
-file_path_read = OS.path.join('Resources','budget_data.csv')
-file_path_write = OS.path.join('analysis','Financial_Analysis.txt')
+file_path_read = OS.path.join('Resources','budget_data.csv')            #Input file
+file_path_write = OS.path.join('analysis','Financial_Analysis.txt')     #Output file
 
+# ----- Put output data in Dictionary ------------------------------------------------
+# ----- The item key's name is the output title 
 output_total_month = 'Total Months'
 output_total = 'Total'
 output_avg_change = 'Average  Change'
 output_GIP = 'Greatest Increase in Profits'
 output_GDP = 'Greatest Decrease in Profits'
 output_dict = {output_total_month:0,output_total:0,output_avg_change:0,output_GIP:'',output_GDP:''}
+#--------------------------------------------------------------------------------------
 
+#Input data column index
 col_month = 0
 col_value = 1
 
 with open(file_path_read,'r') as csv_file:
     csv_reader = CSV.reader(csv_file,delimiter=',')
-    next(csv_reader)
+
+    next(csv_reader)    #Skip first row as it is header
+
     for csv_line in csv_reader:
-        output_dict[output_total_month] += 1
-        int_value = int(csv_line[col_value])
-        output_dict[output_total] += int_value
-        if output_dict[output_GIP] == '':
-            greatest_increase = int_value
+        output_dict[output_total_month] += 1    #Counting number of months
+        int_value = int(csv_line[col_value])    #Store Integer converted value for multiple times uses
+        output_dict[output_total] += int_value  #Counting Profit/Loss values
+
+        # -- If this "TRUE" means this is the first row of data,
+        # -- Then initialize output dictionary by give them the first row values
+        if output_dict[output_GIP] == '':       
+            greatest_increase = int_value       
             greatest_decrease = int_value
             output_dict[output_GIP] = f'{csv_line[col_month]} (${int_value})'
             output_dict[output_GDP] = f'{csv_line[col_month]} (${int_value})'
-        else:
+        else: 
+            # -- If it not the first row 
+            # -- change the MAX and MIN values if found new MAX and MIN
             if int_value > greatest_increase :
                 greatest_increase = int_value
                 output_dict[output_GIP] = f'{csv_line[col_month]} (${int_value})'
@@ -34,10 +45,11 @@ with open(file_path_read,'r') as csv_file:
                 greatest_decrease = int_value
                 output_dict[output_GDP] = f'{csv_line[col_month]} (${int_value})'
     
+    #Calculate average value
     output_dict[output_avg_change] = int(output_dict[output_total] / output_dict[output_total_month])
     
     
-    
+#Output to text file and display    
 with open(file_path_write,'w',newline='') as txt_file :
     txt_writer = CSV.writer(txt_file)
     text_tmp = '\nFinancial Analysis\n'
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,26 +1,32 @@
 import os as OS
 import csv as CSV
 
-file_path_read = OS.path.join('Resources','election_data.csv')
-file_path_write = OS.path.join('analysis','Election_Results.txt')
+file_path_read = OS.path.join('Resources','election_data.csv')      #Input File
+file_path_write = OS.path.join('analysis','Election_Results.txt')   #Output File
 
-output_dict = {}
+output_dict = {}    #Dictionary for output
 
-col_candidate = 2
+col_candidate = 2   #Input data row index
 
-voters = 0
+voters = 0          #Initialize number of vote
 
 with open(file_path_read,'r') as csv_file:
     csv_reader = CSV.reader(csv_file, delimiter=',')
-    next(csv_reader)
+
+    next(csv_reader)    #Skip first row as it is header
+
     for csv_line in csv_reader:
-        voters += 1
+        voters += 1     #Counting number of vote
+
+        # -- Check if the candidate exists in the output dictionary key
+        # -- If not means this is candidate first vote, then add candidate to the kay and add 1 vote to value
         if csv_line[col_candidate] not in output_dict.keys():
             output_dict[csv_line[col_candidate]] = 1
         else:
             output_dict[csv_line[col_candidate]] += 1
 
 
+#Output to text file and display 
 with open(file_path_write,'w',newline='') as txt_file:
     txt_write = CSV.writer(txt_file)
 
